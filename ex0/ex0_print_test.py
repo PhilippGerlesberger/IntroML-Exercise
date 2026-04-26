a3d3e2c2af56e49331f699e89792c8672f5ab846
Philipp Gerlesberger <Gerlesberger.Philipp@gmail.com>
2026-04-26 22:43:15 +0200

    Add sandbox test.

--- ex0/ex0_print_test.py
+++ ex0/ex0_print_test.py
@@ -14,6 +14,13 @@ def create_image(image_name: str) -> tuple[np.ndarray, str]:
     cv2.imwrite(full_image_path, test_image)
     return test_image, full_image_path
 
+def create_simple_image(image_name: str) -> tuple[np.ndarray, str]:
+    # Create a random image, create the full path to store it and then save it using CV2.
+    test_image: np.ndarray = np.ones((100, 100, 3), dtype=np.uint8)
+    full_image_path: str = os.path.join(".\\", os.path.join(IMAGE_PARENT_DIRECTORY, image_name))
+    cv2.imwrite(full_image_path, test_image)
+    return test_image, full_image_path
+
 
 def delete_image(full_image_path: str):
     # Remove the randomly created image.
@@ -21,7 +28,11 @@ def delete_image(full_image_path: str):
         os.remove(full_image_path)
 
 def print_matrix(X):
-    print(X[0, 0:3, ::])
+
+    if X.shape[-1] == 3:
+        print(X[0:3, 0:3, :])
+    else:
+        print(X[:3])
 
 def compare_matrix(A, B):
     print("--------------------------")
@@ -33,7 +44,29 @@ def compare_matrix(A, B):
 
 
 class Tests():
-    
+
+    def dump_matrix(self):
+        test_image, image_path = create_image("test.png")
+        processor: ImageProcessor = ImageProcessor(image_path, "RGB")
+
+        print("1------------------")
+        print_matrix(test_image)
+        print("2------------------")
+        print_matrix(test_image[:,:])
+        print("3------------------")
+        a = np.max(test_image, axis=2)/3
+        print_matrix(a)
+        print("4------------------")
+
+        processor.convert_to_grayscale(method = "average")
+        gray_image, _ = processor.get_image_data()
+        processor.show_image()
+        #print_matrix(gray_image)
+
+        delete_image(image_path)
+
+
+
     def test_init_rgb(self):
         test_image, image_path = create_image("test.png")
 
@@ -51,7 +84,7 @@ class Tests():
 
 
         delete_image(image_path)
-    
+
     def test_init_gray(self):
         test_image, image_path = create_image("test.png")
         test_image_shape: np.ndarray = np.array(test_image.shape)
@@ -64,6 +97,17 @@ class Tests():
 
         delete_image(image_path)
 
+    def test_gray_scale(self):
+        test_image, image_path = create_image("test.png")
+        processor: ImageProcessor = ImageProcessor(image_path, "RGB")
+
+        processor.convert_to_grayscale()
+
+        gray_image, _ = processor.get_image_data()
+
+        compare_matrix(test_image, gray_image)
+
+
     def test_bgr_to_rgb(self):
         test_image, image_path = create_image("test.png")
 
@@ -72,15 +116,20 @@ class Tests():
         image_converted, image_converted_colour_type = processor.get_image_data()
 
         delete_image(image_path)
-    
+
     def test_rgb_to_bgr(self):
         test_image, image_path = create_image("test.png")
 
-        print("test_image")
-        print_matrix(test_image)
         processor: ImageProcessor = ImageProcessor(image_path, "RGB")
+        img, ct = processor.get_image_data()
         processor.convert_colour()
         image_converted, image_converted_colour_type = processor.get_image_data()
+        compare_matrix(test_image, image_converted)
+        compare_matrix(test_image, image_converted[:,:,::-1])
+
+        print(ct, image_converted_colour_type)
+
+
 
         test_image_converted: np.ndarray = cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR)
         compare_matrix(image_converted, test_image_converted)
@@ -103,15 +152,11 @@ class Tests():
         processor: ImageProcessor = ImageProcessor(image_path, "BGR")
         processor.flip_image(0)
         flipped_image, _ = processor.get_image_data()
-        
+
 
         test_image_flipped: np.ndarray = np.fliplr(test_image)
-        print(test_image[0][:3])
-        print("-----------")
-        print(flipped_image[0][:3])
-        print("-----------")
-        print(test_image_flipped[0][:3])
-
+        compare_matrix(test_image, flipped_image)
+        compare_matrix(test_image_flipped, flipped_image)
 
 
         delete_image(image_path)
